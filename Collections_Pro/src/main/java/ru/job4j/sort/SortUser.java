165c6cf8b3838f628e82c26d43d27cc9592c6dbc
AndreyShulgin <neonod404@gmail.com>
2019-07-25 13:11:46 +0300

    2. Сортировка User с использованием Comparator[#80664]

--- Collections_Pro/src/main/java/ru/job4j/sort/SortUser.java
+++ Collections_Pro/src/main/java/ru/job4j/sort/SortUser.java
@@ -1,12 +1,21 @@
 package ru.job4j.sort;
 
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class SortUser {
 
     public Set<User> sort(List<User> users) {
         return new TreeSet<>(users);
+    }
+
+    public List<User> sortNameLength(List<User> users) {
+        users.sort((o1, o2) -> Integer.compare(o2.getName().length(), o1.getName().length()));
+
+        return users;
+    }
+
+    public List<User> sortByAllFields(List<User> users) {
+        users.sort(Comparator.comparing(User::getName).thenComparingInt(User::getAge));
+        return users;
     }
 }
--- Collections_Pro/src/test/java/ru/job4j/sort/SortUserTest.java
+++ Collections_Pro/src/test/java/ru/job4j/sort/SortUserTest.java
@@ -18,4 +18,19 @@ public class SortUserTest {
                 new User("Alex", 25));
         assertThat(new SortUser().sort(list), is(expect));
     }
+
+    @Test
+    public void whenSortListThenNameLength() {
+        List<User> result = new ArrayList<>(List.of(new User("Alex", 25),
+                new User("Andrey", 27)));
+        assertThat(new SortUser().sortNameLength(result).get(0).getName(), is("Andrey"));
+    }
+
+    @Test
+    public void whenSortListThenNameAndAge() {
+        List<User> result = new ArrayList<>(List.of(new User("Alex", 25),
+                new User("Andrey", 27),
+                new User("Alex", 10)));
+        assertThat(new SortUser().sortByAllFields(result).get(0).getAge(), is(10));
+    }
 }
